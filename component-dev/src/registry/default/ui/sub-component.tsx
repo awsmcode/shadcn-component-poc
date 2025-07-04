bc7457208a63ac003c759afe7772a008cbbefc94
AwsmCode <awsmcode@gmail.com>
2025-07-04 15:45:57 +0200

    add doc message

--- component-dev/src/registry/default/ui/sub-component.tsx
+++ component-dev/src/registry/default/ui/sub-component.tsx
@@ -2,7 +2,7 @@ import { Badge } from "@/components/ui/badge"
 
 export function SubComponent() {
     return <div>
-<h1 className="text-2xl font-bold" style={{ color: 'var(--myvar)', backgroundColor: "blue" }}>
+<h1 className="text-2xl font-bold bg-[var(--mytestvar)]" style={{ color: 'var(--myvar)' }}>
         Hello World (2) (sub-component)</h1>
 
 <Badge>Content Badge</Badge>
