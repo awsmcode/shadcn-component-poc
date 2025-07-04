af77345e962ead1762f3d5b9327520e8f8203c95
AwsmCode <awsmcode@gmail.com>
2025-07-04 13:53:06 +0200

    registry added

--- component-dev/src/App.tsx
+++ component-dev/src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import { Button } from './components/ui/button'
+import { SubComponent } from './registry/default/ui/sub-component'
 
 
 function App() {
-
-
   return (
     <>
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+        <SubComponent />
+      </div>
     </>
   )
 }
---
+++ component-dev/src/registry/default/ui/sub-component.tsx
@@ -0,0 +1,5 @@
+export function SubComponent() {
+    return <h1 className="text-2xl font-bold">
+        Hello World (2) (sub-component)</h1>
+}
+/* argus terraform */
