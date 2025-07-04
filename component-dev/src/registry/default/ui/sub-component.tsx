1986d1a914d1ddddfc8ca3df0b8fb59fec1b746d
AwsmCode <awsmcode@gmail.com>
2025-07-04 14:52:58 +0200

    add own css vars to registry & component

--- component-dev/src/registry/default/ui/sub-component.tsx
+++ component-dev/src/registry/default/ui/sub-component.tsx
@@ -1,5 +1,12 @@
+import { Badge } from "@/components/ui/badge"
+
 export function SubComponent() {
-    return <h1 className="text-2xl font-bold">
+    return <div>
+<h1 className="text-2xl font-bold" style={{ color: 'var(--myvar)', backgroundColor: "blue" }}>
         Hello World (2) (sub-component)</h1>
+
+<Badge>Content Badge</Badge>
+
+    </div> 
 }
 /* argus terraform */
